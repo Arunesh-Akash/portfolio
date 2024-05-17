03177b776c936c6518216c6a1fc2579971ddea16
Arunesh Akash <akashkr099@gmail.com>
2024-05-17 22:27:43 +0530

    Correct the image container

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,37 +5,34 @@ import { HiOutlineMail } from 'react-icons/hi'
 
 const Contact = () => {
     return (
-        <div name="contact" className="w-full bg-gradient-to-b from-black via-black to-gray-800 ">
+        <div name="contact" className="w-full bg-gradient-to-b from-black via-black to-gray-800">
             <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full text-white">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">
                         Contact
                     </p>
                 </div>
-                <div className='flex justify-center items-center gap-20 mt-10'>
-                <a className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2' href='https://github.com/Arunesh-Akash'>
-
-                    <FaGithub />
-                    <p>GitHub</p>
-
-                </a>
-                <a className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2' href='https://www.linkedin.com/in/arunesh-akash-a358061bb'>
-                    <FaLinkedin />
-                    <p>LinkedIn</p>
-                </a>
-                <a className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2' href="mailto:akashkr099@gmail.com">
-                    <HiOutlineMail />
-                    <p>E-Mail</p>
-                </a>
-                <div className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2'>
-                    <FaPhoneAlt />
-                    <p>+91-9955235611</p>
+                <div className='flex flex-col md:flex-row justify-center items-center gap-6 mt-10'>
+                    <a className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2' href='https://github.com/Arunesh-Akash'>
+                        <FaGithub size={25} />
+                        <p>GitHub</p>
+                    </a>
+                    <a className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2' href='https://www.linkedin.com/in/arunesh-akash-a358061bb'>
+                        <FaLinkedin size={25} />
+                        <p>LinkedIn</p>
+                    </a>
+                    <a className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2' href="mailto:akashkr099@gmail.com">
+                        <HiOutlineMail size={25} />
+                        <p>E-Mail</p>
+                    </a>
+                    <div className='flex justify-center items-center gap-3 hover:bg-gray-700 rounded-lg p-2'>
+                        <FaPhoneAlt size={25} />
+                        <p>+91-9955235611</p>
+                    </div>
                 </div>
-
-                </div>
-                <div className='flex justify-center items-center flex-row gap-2 mt-2'>
+                <div className='flex justify-center items-center flex-row gap-2 mt-10 text-center'>
                     <span>Copyright</span>
-                    <span><MdCopyright size={15}/></span>
+                    <span><MdCopyright size={15} /></span>
                     <span className='text-blue-600'>Arunesh Akash</span>
                     <span> - All rights reserved 2024</span>
                 </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image from '../assets/heroImage.jpeg'
-import { MdOutlineArrowRightAlt } from "react-icons/md";
-import { MdOutlineFileDownload } from "react-icons/md";
+import { MdOutlineArrowRightAlt, MdOutlineFileDownload } from "react-icons/md";
 import { Link } from 'react-scroll';
 import Typewriter from 'typewriter-effect';
 
@@ -12,15 +11,15 @@ const Home = () => {
             className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
         >
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-between h-full px-4 md:flex-row">
-                <div>
+                <div className="w-full md:w-1/2 flex justify-center md:justify-start">
                     <img
                         src={Image}
                         alt="my profile"
-                        className="rounded-2xl mx-auto w-20 md:w-full"
+                        className="rounded-2xl w-52 md:w-72 lg:w-96" // Set fixed widths for different breakpoints
                         style={{ marginTop: '29px' }}
                     />
                 </div>
-                <div className="flex flex-col justify-center h-full ml-10">
+                <div className="flex flex-col justify-center h-full mt-6 md:mt-0 md:ml-10 w-full md:w-1/2">
                     <h2 className="text-4xl sm:text-7xl font-bold text-white">
                         I'm a <Typewriter
                             options={{
@@ -31,7 +30,7 @@ const Home = () => {
                         />
                     </h2>
                     <p className="text-gray-500 py-4 max-w-md">
-                    My passion for coding and problem-solving motivates me to continuously learn and stay up-to-date with the latest technologies and industry trends. I am a collaborative team player and have excellent communication skills, making me a valuable asset to any project.
+                        My passion for coding and problem-solving motivates me to continuously learn and stay up-to-date with the latest technologies and industry trends. I am a collaborative team player and have excellent communication skills, making me a valuable asset to any project.
                     </p>
 
                     <div className='flex gap-5'>
@@ -40,7 +39,6 @@ const Home = () => {
                             <span className="group-hover:rotate-90 duration-300 ">
                                 <MdOutlineArrowRightAlt size={25} className="ml-1" />
                             </span>
-
                         </Link>
 
                         <a href="/Arunesh's resume.pdf" download={true} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
@@ -48,13 +46,9 @@ const Home = () => {
                                 <MdOutlineFileDownload size={15} className='mr-1' />
                             </span>
                             Download CV
-
                         </a>
-
                     </div>
                 </div>
-
-
             </div>
         </div>
     );
